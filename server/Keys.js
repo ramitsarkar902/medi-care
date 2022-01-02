64733911116da740603a8f00b68346bc3c41e2fd
Ramit <ramitsarkar902@gmail.com>
2022-01-03 01:47:39 +0530

    del

--- server/Keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoUrl:
-    'mongodb+srv://ggg:ggg@cluster0.cen7f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-   jwtKey:'ManiaplManipal' 
-};
--- src/src/context/InitialState.js
+++
@@ -1,6 +0,0 @@
-export const INITIAL_STATE = {
-  user: null,
-  token: '',
-  isFetching: false,
-  error: false,
-};
